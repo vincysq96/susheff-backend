c17d3ac10238a365b24920586d0e9b264a2ad652
Vinzenzo Incorvaia <vincenzo.incorvaia@external.nexigroup.com>
2026-02-03 14:57:01 +0100

    Cors + security

--- src/main/java/com/vincenzo/susheff/core/configurations/CorsConfig.java
+++ src/main/java/com/vincenzo/susheff/core/configurations/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.vincenzo.susheff.core.configurations;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,11 +12,15 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
+
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of(allowedOrigins.split(",")));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
--- src/main/java/com/vincenzo/susheff/core/configurations/SecurityConfig.java
+++ src/main/java/com/vincenzo/susheff/core/configurations/SecurityConfig.java
@@ -36,9 +36,11 @@ public class SecurityConfig {
   SecurityFilterChain configure(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
     http
       .cors(cors -> cors.configurationSource(corsConfigurationSource))
-      .csrf(AbstractHttpConfigurer::disable);
+      .csrf(AbstractHttpConfigurer::disable)
+      .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
     return http.build();
   }
+
 
 }
